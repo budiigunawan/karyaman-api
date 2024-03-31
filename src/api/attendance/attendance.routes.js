80509c8444e411fd202df93f85d4af5e73b131b3
Budi Gunawan <budiindra.g@gmail.com>
2024-03-31 20:24:55 +0700

    feat: add findAttendances function

--- src/api/attendance/attendance.routes.js
+++ src/api/attendance/attendance.routes.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const { isAuthenticated } = require('../middlewares');
-const { createAttendance } = require('./attendance.services');
+const {
+  createAttendance,
+  findAttendances,
+  findAttendancesByUserId,
+  countAttendances,
+  countAttendancesByUserId,
+} = require('./attendance.services');
+const { findUserById } = require('../users/users.services');
 
 const router = express.Router();
 
@@ -19,6 +26,32 @@ router.post('/create', isAuthenticated, async (req, res, next) => {
       message: 'Attendance created successfully',
       data: newAttendance,
     });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/list', isAuthenticated, async (req, res, next) => {
+  try {
+    const { userId } = req.payload;
+    const { page = 1, limit = 10 } = req.query;
+    const user = await findUserById(userId);
+
+    let attendances;
+    let totalData;
+    let totalPage;
+
+    if (user.isAdmin) {
+      attendances = await findAttendances(page, limit);
+      totalData = await countAttendances();
+      totalPage = Math.ceil(totalData / limit);
+    } else {
+      attendances = await findAttendancesByUserId(page, limit, userId);
+      totalData = await countAttendancesByUserId(userId);
+      totalPage = Math.ceil(totalData / limit);
+    }
+
+    res.json({ totalData, totalPage, data: attendances });
   } catch (err) {
     next(err);
   }
--- src/api/attendance/attendance.services.js
+++ src/api/attendance/attendance.services.js
@@ -9,6 +9,61 @@ function createAttendance(attendance) {
   });
 }
 
+function countAttendancesByUserId(userId) {
+  return db.attendance.count({
+    where: {
+      userId,
+    },
+  });
+}
+
+function countAttendances() {
+  return db.attendance.count();
+}
+
+function findAttendancesByUserId(page, limit, userId) {
+  const take = Number(limit);
+  const skip = (page - 1) * limit;
+
+  return db.attendance.findMany({
+    take,
+    skip,
+    where: {
+      userId,
+    },
+    include: {
+      user: {
+        select: {
+          fullName: true,
+          role: true,
+        },
+      },
+    },
+  });
+}
+
+function findAttendances(page, limit) {
+  const take = Number(limit);
+  const skip = (page - 1) * limit;
+
+  return db.attendance.findMany({
+    take,
+    skip,
+    include: {
+      user: {
+        select: {
+          fullName: true,
+          role: true,
+        },
+      },
+    },
+  });
+}
+
 module.exports = {
   createAttendance,
+  findAttendancesByUserId,
+  findAttendances,
+  countAttendancesByUserId,
+  countAttendances,
 };
