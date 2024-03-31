a1589d21869c0e8382c9c05c4a979a8acb8a0797
Budi Gunawan <budiindra.g@gmail.com>
2024-03-31 11:01:21 +0700

    feat: add refresh token

--- src/api/auth/auth.routes.js
+++ src/api/auth/auth.routes.js
@@ -2,15 +2,22 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const bcrypt = require('bcrypt');
-const { generateTokens } = require('../../utils/jwt');
-const { addRefreshTokenToWhitelist } = require('./auth.services');
-// const jwt = require('jsonwebtoken');
-
-const router = express.Router();
+const jwt = require('jsonwebtoken');
 const {
   findUserByEmail,
   createUserByEmailAndPassword,
+  findUserById,
 } = require('../users/users.services');
+const { generateTokens } = require('../../utils/jwt');
+const {
+  addRefreshTokenToWhitelist,
+  findRefreshTokenById,
+  deleteRefreshToken,
+  revokeTokens,
+} = require('./auth.services');
+const { hashToken } = require('../../utils/hashToken');
+
+const router = express.Router();
 
 router.post('/register', async (req, res, next) => {
   try {
@@ -73,6 +80,54 @@ router.post('/login', async (req, res, next) => {
     res.json({
       accessToken,
       refreshToken,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/refreshToken', async (req, res, next) => {
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+      res.status(400);
+      throw new Error('Missing refresh token.');
+    }
+    const payload = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
+    const savedRefreshToken = await findRefreshTokenById(payload.jti);
+
+    if (!savedRefreshToken || savedRefreshToken.revoked === true) {
+      res.status(401);
+      throw new Error('Unauthorized');
+    }
+
+    const hashedToken = hashToken(refreshToken);
+    if (hashedToken !== savedRefreshToken.hashedToken) {
+      res.status(401);
+      throw new Error('Unauthorized');
+    }
+
+    const user = await findUserById(payload.userId);
+    if (!user) {
+      res.status(401);
+      throw new Error('Unauthorized');
+    }
+
+    await deleteRefreshToken(savedRefreshToken.id);
+    const jti = uuidv4();
+    const { accessToken, refreshToken: newRefreshToken } = generateTokens(
+      user,
+      jti,
+    );
+    await addRefreshTokenToWhitelist({
+      jti,
+      refreshToken: newRefreshToken,
+      userId: user.id,
+    });
+
+    res.json({
+      accessToken,
+      refreshToken: newRefreshToken,
     });
   } catch (err) {
     next(err);
