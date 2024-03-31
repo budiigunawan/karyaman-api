bb03841700cce89a789c294575fd09585865b2c0
Budi Gunawan <budiindra.g@gmail.com>
2024-03-31 11:14:01 +0700

    feat: add middlewares

---
+++ src/api/middlewares.js
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const jwt = require('jsonwebtoken');
+
+function isAuthenticated(req, res, next) {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    res.status(401);
+    throw new Error('Un-Authorized');
+  }
+
+  try {
+    const token = authorization.split(' ')[1];
+    const payload = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+    req.payload = payload;
+  } catch (err) {
+    res.status(410);
+
+    if (err.name === 'TokenExpiredError') {
+      throw new Error(err.name);
+    }
+
+    throw new Error('Un-Authorized');
+  }
+
+  return next();
+}
+
+module.exports = {
+  isAuthenticated,
+};
